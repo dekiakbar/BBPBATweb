dfad8245077934b6a46930fab69aa3bd2fb7398b
dekiakbar <dekiakbar@linuxmail.org>
2018-03-19 20:38:22 +0700

    sub domain route blog

--- routes/web.php
+++ routes/web.php
@@ -11,64 +11,74 @@
 |
 */
 
-Route::get('/', 'WebController@tampil');
-Route::get('kategori/{kategori}','WebController@kategori');
-Route::get('tag/{tag}','WebController@tag');
-Route::post('/','WebController@cari');
-Route::get('detail/{slug}','WebController@detail');
-
 /*
 |--------------------------------------------------------------------------
-| Route Admin login dan CRUD
+| Route subdomain blog 
 |--------------------------------------------------------------------------
 |
 */
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'] ,function () {
-	Route::get('/', 'HomeController@index')->name('home');
-	Route::post('/','HomeController@gantiPas');
+Route::domain('blog.bbpbat.go.id')->group(function () {
 
-	// Registrasi Admin
-	Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-	Route::post('register', 'Auth\RegisterController@register');
+	Route::get('/', 'WebController@tampil');
+	Route::get('kategori/{kategori}','WebController@kategori');
+	Route::get('tag/{tag}','WebController@tag');
+	Route::post('/','WebController@cari');
+	Route::get('detail/{slug}','WebController@detail');
 
-	//Routing kategori
-	Route::resource('/kategori','KategoriController',[
-	    'except' => 'show'
-	]);
-	Route::post('/kategori/cari','KategoriController@cari')->name('kategori.cari');
+	/*
+	|--------------------------------------------------------------------------
+	| Route Admin login dan CRUD
+	|--------------------------------------------------------------------------
+	|
+	*/
+	Route::group(['prefix' => 'admin', 'middleware' => 'auth'] ,function () {
+		Route::get('/', 'HomeController@index')->name('home');
+		Route::post('/','HomeController@gantiPas');
 
-	//Routing tag
-	Route::resource('/tag','TagController',[
-		'except' => 'show'
-	]);
-	Route::post('/tag/cari','TagController@cari')->name('tag.cari');
+		// Registrasi Admin
+		Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+		Route::post('register', 'Auth\RegisterController@register');
 
-	//route artikel
-	Route::resource('/artikel','ArtikelController');
-	Route::post('/artikel/cari','ArtikelController@cari')->name('artikel.cari');
-	
-	//route json kategori
-	Route::get('tag/{kategori_id}', 'TagController@ambilTag');
+		//Routing kategori
+		Route::resource('/kategori','KategoriController',[
+		    'except' => 'show'
+		]);
+		Route::post('/kategori/cari','KategoriController@cari')->name('kategori.cari');
+
+		//Routing tag
+		Route::resource('/tag','TagController',[
+			'except' => 'show'
+		]);
+		Route::post('/tag/cari','TagController@cari')->name('tag.cari');
+
+		//route artikel
+		Route::resource('/artikel','ArtikelController');
+		Route::post('/artikel/cari','ArtikelController@cari')->name('artikel.cari');
+		
+		//route json kategori
+		Route::get('tag/{kategori_id}', 'TagController@ambilTag');
+
+	});
+
+	/*
+	|--------------------------------------------------------------------------
+	| Override Route Autentifikasi User
+	|--------------------------------------------------------------------------
+	|
+	*/
+
+	//Route Login
+	Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+	Route::post('login', 'Auth\LoginController@login');
+	Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+	// Lupa password
+	Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+	Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+	Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+	Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 });
-
-/*
-|--------------------------------------------------------------------------
-| Override Route Autentifikasi User
-|--------------------------------------------------------------------------
-|
-*/
-
-//Route Login
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-
-// Lupa password
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 /*
 |--------------------------------------------------------------------------
