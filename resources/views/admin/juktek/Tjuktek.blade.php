e86b320d8c1e3ca4a19a750ed7ab3af314a21df9
dekiakbar <dekiakbar@linuxmail.org>
2018-03-23 10:54:09 +0700

    fix idex juktek

--- resources/views/admin/juktek/Tjuktek.blade.php
+++ resources/views/admin/juktek/Tjuktek.blade.php
@@ -1,4 +1,4 @@
-@extends('master')
+@extends('admin.master')
 @section('judul','Tambah Petunjuk Teknis')
 @section('isi')
 <div class="ui stackable centered grid">
@@ -16,22 +16,31 @@
 	   <div class="ui segment">
 			<form class="ui form">
 	  			<div class="three fields">
+
 	    			<div class="seven wide field">
 	    			  	<label>Judul Petunjuk</label>
 	    			  	<input placeholder="Judul Petunjuk Teknis" name="judul" type="text" id="dataSlug" onkeyup="buatSlug()">
 	    			</div>
+
 	    			<div class="seven wide field">
 	    				<label>Slug</label>
-	    		 		<input type="text" name="slug" id="slug" disabled placeholder="Untuk URL">
+	    		 		<input type="text" name="slug" id="slug" disabled placeholder="Untuk URL Petunjuk Teknis">
 	    			</div>
+
 	    			<div class="two wide field">
-	    				<input type="file" (change)="fileEvent($event)" name="foto" id="labelinput" style="opacity: 0;overflow: hidden;margin-top: -28px;" />
+	    				<input type="file" name="foto" id="labelinput" style="opacity: 0;overflow: hidden;margin-top: -28px;" />
 						<label for="labelinput" class="ui huge green button">
 							<i class="ui upload icon"></i> 
-							Upload image
+							Upload Foto
 						</label>
 	    			</div>
 	      		</div>
+
+	      		<div class="field">
+	      			<label>Kutipan</label>
+	      			<textarea name="kutipan"></textarea>
+	      		</div>
+
 	      		<div class="field">
       				<label>Masukan Petunjuk Teknis</label>
       				<textarea name="artikel"></textarea>
@@ -39,7 +48,7 @@
 
 				<div class="ui stackable centered grid">
     				<div class="six wide column center aligned">
-	 					<button type="submit" class="ui button" tabindex="0">Simpan</button>
+	 					<button type="submit" class="ui button blue" tabindex="0">Simpan</button>
 					</div>
 				</div>
 
