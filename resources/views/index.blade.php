6535225c17ac7b73d67c005c4a33672b6dab4691
dekiakbar <dekiakbar@linuxmail.org>
2018-03-29 10:59:17 +0700

    remove inverted segment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -224,51 +224,51 @@
     <div class="ui segment">
       	<div class="ui special cards">
 			<div class="card">
-			  <div class="blurring dimmable image">
-			    <div class="ui dimmer">
-			      <div class="content">
-			        <div class="center">
-			          <div class="ui inverted button" style="margin-top: 50%;">Selanjutnya</div>
-			        </div>
-			      </div>
-			    </div>
-			    <img src="images/7.jpeg">
-			  <div class="content">
-			    <a class="header">Judul Artikel</a>
-			    <div class="meta">
-			      <span class="date">Created in Sep 2014</span>
-			    </div>
-			  </div>	  
-			</div>
+			  	<div class="blurring dimmable image">
+			    	<div class="ui dimmer">
+			    	  	<div class="content">
+			    	    	<div class="center">
+			    	      		<div class="ui inverted button" style="margin-top: 50%;">Selanjutnya</div>
+			    	    	</div>
+			    	  	</div>
+			    	</div>
+			    	<img src="images/7.jpeg">
+			  		<div class="content">
+			  		  	<a class="header">Judul Artikel</a>
+			  		  	<div class="meta">
+			  		    	<span class="date">Created in Sep 2014</span>
+			  		  	</div>
+			  		</div>	  
+				</div>
 			</div>
     	</div>
   	</div>
   </div>
 
-<div class="column">
-    <div class="ui segment">
-      <div class="ui special cards">
-		  <div class="card">
-		    <div class="blurring dimmable image">
-		      <div class="ui dimmer">
-		        <div class="content">
-		          <div class="center">
-		            <div class="ui inverted button" style="margin-top: 50%;">Selanjutnya</div>
-		          </div>
-		        </div>
-		      </div>
-		      <img src="images/7.jpeg">
-		    <div class="content">
-		      <a class="header">Judul Artikel</a>
-		      <div class="meta">
-		        <span class="date">Created in Sep 2014</span>
-		      </div>
-		    </div>	  
-		  </div>
-		 </div>
-    </div>
-  </div>
-  </div>
+	<div class="column">
+	    <div class="ui segment">
+	      <div class="ui special cards">
+			  <div class="card">
+			    <div class="blurring dimmable image">
+			      <div class="ui dimmer">
+			        <div class="content">
+			          <div class="center">
+			            <div class="ui inverted button" style="margin-top: 50%;">Selanjutnya</div>
+			          </div>
+			        </div>
+			      </div>
+			      <img src="images/7.jpeg">
+			    <div class="content">
+			      <a class="header">Judul Artikel</a>
+			      <div class="meta">
+			        <span class="date">Created in Sep 2014</span>
+			      </div>
+			    </div>	  
+			  </div>
+			 </div>
+	      </div>
+	    </div>
+	</div>
 
 
 <div class="column">
@@ -303,7 +303,7 @@
 
 
 <div class="ui segment">
-<div class="ui inverted segment">
+<div class="ui segment">
 	<div class="ui centered grid">
 	<br>
 <h2>Petunjuk Teknis</h2>
@@ -553,71 +553,35 @@ function showDivs(n) {
 </script>
 	
 	<script type="text/javascript">	
-			$(function() {
-			
-				var Page = (function() {
-
-					var $nav = $( '#nav-dots > span' ),
-						slitslider = $( '#slider' ).slitslider( {
-							onBeforeChange : function( slide, pos ) {
-
-								$nav.removeClass( 'nav-dot-current' );
-								$nav.eq( pos ).addClass( 'nav-dot-current' );
-
-							}
-						} ),
-
-						init = function() {
-
-							initEvents();
-							
-						},
-						initEvents = function() {
-
-							$nav.each( function( i ) {
-							
-								$( this ).on( 'click', function( event ) {
-									
-									var $dot = $( this );
-									
-									if( !slitslider.isActive() ) {
-
-										$nav.removeClass( 'nav-dot-current' );
-										$dot.addClass( 'nav-dot-current' );
-									
-									}
-									
-									slitslider.jump( i + 1 );
-									return false;
-								
-								} );
-								
+		$(function() {
+			var Page = (function() {
+				var $nav = $( '#nav-dots > span' ),
+					slitslider = $( '#slider' ).slitslider( {
+						onBeforeChange : function( slide, pos ) {
+							$nav.removeClass( 'nav-dot-current' );
+							$nav.eq( pos ).addClass( 'nav-dot-current' );
+						}
+					} ),
+					init = function() {
+						initEvents();							
+					},
+					initEvents = function() {
+						$nav.each( function( i ) {
+							$( this ).on( 'click', function( event ) {
+								var $dot = $( this );
+								if( !slitslider.isActive() ) {
+									$nav.removeClass( 'nav-dot-current' );
+									$dot.addClass( 'nav-dot-current' );
+								}
+								slitslider.jump( i + 1 );
+								return false;
 							} );
-
-						};
-
-						return { init : init };
-
-				})();
-
-				Page.init();
-
-				/**
-				 * Notes: 
-				 * 
-				 * example how to add items:
-				 */
-
-				/*
-				
-				var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
-				
-				// call the plugin's add method
-				ss.add($items);
-
-				*/
-			
-			});
-		</script>
+						} );
+					};
+					return { init : init };
+			})();
+			Page.init();
+		});
+	</script>
 </body>
 </html>
